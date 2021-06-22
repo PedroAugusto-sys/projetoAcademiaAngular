bb6f3badd8614438d03cf3a33fc3ba6dff14ad28
PedroAugusto-sys <pedrocaandrade01@gmail.com>
2021-06-22 20:58:25 -0300

    a

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {HttpClientModule} from '@angular/common/http';
+import {AppRoutingModule} from '../../sistemaAcademia-web/src/app/app-routing.module';
+import {CadastrePatientComponent} from '../../sistemaAcademia-web/src/app/cliente/cadastroCliente/cadastro-cliente.component';
+import {CadastreComponent} from '../../sistemaAcademia-web/src/app/services/cadastre/cadastre.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CadastrePatientComponent,
+    CadastreComponent
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    HttpClientModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
